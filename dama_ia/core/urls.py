60c38476df938e92ab1ed2def89dff5f1ba375ef
Alex Jimenez <alexjimenez.az46@gmail.com>
2025-09-24 23:15:44 -0600

    Feat: Se agrego la ruta del endpoint clear_chat_history a urls.py

--- dama_ia/core/urls.py
+++ dama_ia/core/urls.py
@@ -3,4 +3,5 @@ from .views import generate_response
 
 urlpatterns = [
     path('generate/', generate_response, name='generate_response'),
+    path('clear_chat_history/<str:user_id>/', generate_response, name='clear_chat_history'),
 ]
