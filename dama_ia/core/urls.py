6e04ee4ff2110466b3edafb99353d698367b73c3
Alex Jimenez <alexjimenez.az46@gmail.com>
2025-09-26 01:47:29 -0600

    Feat: Se agrego el path del nuevo endpoint para analizar imagenes al archivo urls.py

--- dama_ia/core/urls.py
+++ dama_ia/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import generate_response, clear_chat_history, get_chat_history
+from .views import generate_response, clear_chat_history, get_chat_history, analyze_image_view
 
 urlpatterns = [
     path('generate/', generate_response, name='generate_response'),
     path('chat/history/clear/<str:user_id>/', clear_chat_history, name='clear_chat_history'),
     path('chat/history/<str:user_id>/', get_chat_history, name='get_chat_history'),
+    path('analyze/image/', analyze_image_view, name='analyze_image_view'),
 ]
