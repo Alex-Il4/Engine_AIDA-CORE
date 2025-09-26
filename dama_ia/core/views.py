24bb54f06da4e2bb40103c4ca8fa52c1e294a6aa
Alex Jimenez <alexjimenez.az46@gmail.com>
2025-09-25 23:53:44 -0600

    Fix: Se soluciono un bug en el ednpoint para ver historial

--- dama_ia/core/views.py
+++ dama_ia/core/views.py
@@ -159,29 +159,29 @@ def clear_chat_history(request, user_id):
             status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
     #Iincio del endpoint para ver historial
-    @api_view(['GET'])
-    def get_chat_history(request, user_id):
-        """
-        Vista que devuelve el historial de chat para un user_id específico.
-        """
-        try:
-            # Obtener los registros que coinciden con el user_id
-            records = ChatHistory.objects.filter(session_id=user_id)
+@api_view(['GET'])
+def get_chat_history(request, user_id):
+    """
+    Vista que devuelve el historial de chat para un user_id específico.
+    """
+    try:
+        # Obtener los registros que coinciden con el user_id
+        records = ChatHistory.objects.filter(session_id=user_id)
 
             # Convertir los registros a un diccionario
-            records_dict = [{
-                'user_message': record.user_message,
-                'bot_response': record.bot_response,
-                'timestamp': record.timestamp
-            } for record in records]
+        records_dict = [{
+            'user_message': record.user_message,
+            'bot_response': record.bot_response,
+            'timestamp': record.timestamp
+        } for record in records]
 
-            return Response({
-                "records": records_dict,
-                "status": "success"
-            })
+        return Response({
+            "records": records_dict,
+            "status": "success"
+        })
 
-        except Exception as e:
-            return Response(
-                {"error": "Ocurrió un error al intentar obtener el historial.", "details": str(e)},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )   
+    except Exception as e:
+        return Response(
+            {"error": "Ocurrió un error al intentar obtener el historial.", "details": str(e)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )   
